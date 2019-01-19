520202dd00b39781e51f612fd71270c7922dd066
Puttipong Wongrak <puttipong.wongrak@gmail.com>
2019-01-19 22:28:47 +0700

    complete lesson create dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 // This is the Link API
 import Layout from '../components/MyLayout'
+import Link from 'next/link'
+
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
 
 const Index = () => (
   <div>
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   </div>
 )
