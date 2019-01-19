935745b5aa83441ba504dd34e0b2adbbdba60adc
Puttipong Wongrak <puttipong.wongrak@gmail.com>
2019-01-19 21:53:35 +0700

    complete lesson navigate between pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+// This is the Link API
+import Link from 'next/link'
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <button style={{ fontSize: 20 }}>About Page</button>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 )
